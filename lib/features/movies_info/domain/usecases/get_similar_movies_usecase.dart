a8f87d6483e1033a3b192717d1a49959ab0c2edd
Nguyễn Văn Bến <nguyenvanben02001@gmail.com>
2024-01-12 21:53:36 +0700

    thêm get_similar_movies_usacse và get_recommend_movies_uscase

---
+++ lib/features/movies_info/domain/usecases/get_similar_movies_usecase.dart
@@ -0,0 +1,23 @@
+import 'package:movie_app/core/resources/data_state.dart';
+import 'package:movie_app/core/usecases/usecase.dart';
+import 'package:movie_app/features/movies_info/data/models/movie_model.dart';
+import 'package:movie_app/features/movies_info/data/models/movie_request.dart';
+import 'package:movie_app/features/movies_info/data/models/my_response.dart';
+import 'package:movie_app/features/movies_info/domain/repository/movie_repository.dart';
+
+class GetSimilarMoviesUseCase
+    implements UseCase<DataState<MyResponse<List<MovieModel>>>, MovieRequest> {
+  final MovieRepository _movieRepository;
+
+  const GetSimilarMoviesUseCase(this._movieRepository);
+
+  @override
+  Future<DataState<MyResponse<List<MovieModel>>>> call({
+    MovieRequest params = const MovieRequest(id: 0, page: 0),
+  }) async {
+    return await _movieRepository.getSimilarMovies(
+      id: params.id,
+      page: params.page,
+    );
+  }
+}
